61308045b55858de51faab809e758908652338f0
nikita <pyatkin.nikitka@mail.ru>
2023-05-21 20:00:01 +0300

    Initial commit

---
+++ src/main/java/com/example/rail_test/RailTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rail_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rail_test/RailTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rail_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
